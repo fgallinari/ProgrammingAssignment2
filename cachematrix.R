3d9604fbff886282b21d798ba4c259f0c60a0e7f
Felipe Gallinari <felipe.gallinari@gmail.com>
2015-10-19 00:27:56 -0200

    Created makeCacheMatrix and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are used to cache squared matrix
+## inversion. Avoiding costly unnecessary computation.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "matrix", which is really
+## a list containing functions to:
+## 1. set the data of the matrix
+## 2. get the data of the matrix
+## 3. set the data of the inverted matrix (solved)
+## 4. get the data of the inverted matrix (solved)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## First time creating, set the solved as NULL
+  s <- NULL
+  
+  ## setDate is used to update the data of the "matrix"
+  ## set the solved back to NULL
+  setData <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  
+  ## return the data of the matrix
+  getData <- function () x
+  
+  ## setSolved is used to set the data of the solved
+  setSolved <- function (solved) s <<- solved
+  
+  ## return the data of the solved
+  getSolved <- function () s
+  
+  ## list of possible functions to call
+  list(setData = setData, 
+       getData = getData,
+       setSolved = setSolved,
+       getSolved = getSolved)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the inverted matrix (solved) of
+## the special matrix created with the above function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Get the inverted matrix of 'x'
+  s <- x$getSolved()
+  
+  ## Check to see if there is a cached value
+  if (!is.null(s)) {
+    ## If so, return the cached value that is the inverse of 'x'
+    return(s)
+  }
+  
+  ## Otherwise, calculate it for the first time
+  data <- x$getData()
+  s <- solve(data)
+  
+  ## And then, save it in the cache
+  x$setSolved(s)
+  
+  ## Return a matrix that is the inverse of 'x'
+  s
 }
